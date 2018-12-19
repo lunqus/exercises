2fe8086e8329289a33208779e449c4dfa3ce9cab
LUNKUS <sigitas@lunkus.com>
2018-12-19 06:00:20 +0200

    url content reader

---
+++ src/main/java/urlContentReader/Main.java
@@ -0,0 +1,15 @@
+package urlContentReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Nusiskaitom puslapi
+        StringBuilder html = new UrlContentReader().readContent("https://www.delfi.lt/");
+
+        // Darom ka reik su juo...
+        System.out.println(html);
+
+    }
+
+}
