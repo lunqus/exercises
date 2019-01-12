d6b5123200ab7111c0f5f60890151c887c8dd4f5
LUNKUS <sigitas@lunkus.com>
2019-01-12 11:19:53 +0200

    adding polymorphic arguments to Vet class

--- src/main/java/polymorphism/MainAnimal.java
+++ src/main/java/polymorphism/MainAnimal.java
@@ -4,8 +4,13 @@ public class MainAnimal {
 
     public static void main(String[] args) {
 
+        Vet myVet = new Vet();
 
+        Cat myCat = new Cat();
         Dog myDog = new Dog();
+
+        myVet.giveShot(myCat);
+        myVet.giveShot(myDog);
 
 //        myDog.makeSound();
 //        myDog.makeSound("Wooff!");
