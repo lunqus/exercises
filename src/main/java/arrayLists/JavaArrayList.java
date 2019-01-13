6c542840f4171482a2daeb691e8cc5e4fb73f581
LUNKUS <sigitas@lunkus.com>
2019-01-13 19:20:53 +0200

    create array list, add elements & check size

--- src/main/java/arrayLists/JavaArrayList.java
+++ src/main/java/arrayLists/JavaArrayList.java
@@ -1,6 +1,18 @@
 package arrayLists;
 
+import java.util.ArrayList;
+
 public class JavaArrayList {
 
+    public static void main(String[] args) {
+        // Array Lists
 
+        ArrayList<String> animals = new ArrayList<>();
+        animals.add("Dog");
+        animals.add("Monkey");
+        animals.add("Cat");
+        animals.add("Donkey");
+
+        System.out.println("Size: " + animals.size());
+    }
 }
