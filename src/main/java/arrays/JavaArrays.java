f5264713439480baee9741313db38a55708ec0ce
LUNKUS <sigitas@lunkus.com>
2019-01-13 10:58:50 +0200

    added elements to newly created empty arrays (also called as blueprints)

--- src/main/java/arrays/JavaArrays.java
+++ src/main/java/arrays/JavaArrays.java
@@ -43,6 +43,35 @@ public class JavaArrays {
         int[] newArray          = new int[5];
         String[] newStringArray = new String[5];
 
+        // Adding elements to an Array
+        newArray[0] = 3;
+        newArray[1] = 4;
+        newArray[2] = 6;
+        newArray[3] = 1;
+        newArray[4] = 12;
+
+        newStringArray[0] = "Marcos";
+        newStringArray[1] = "James";
+        newStringArray[2] = "Jason";
+        newStringArray[3] = "Laura";
+        newStringArray[4] = "Bonni";
+
+        System.out.println("------------ ------------");
+
+        // Loops through newArray ant prints newly specified values
+
+        for (int i = 0; i < newArray.length; i++) {
+
+            System.out.println("Item # " + newArray[i]);
+        }
+
+        System.out.println("------------ ------------");
+
+        for (int i = 0; i < newStringArray.length; i++) {
+
+            System.out.println("Name: " + newStringArray[i]);
+        }
+
     }
 
 }
