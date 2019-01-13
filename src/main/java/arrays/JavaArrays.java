61b2abb865c04ee7e82b669ade06363b98c23bf4
LUNKUS <sigitas@lunkus.com>
2019-01-13 08:23:35 +0200

    java arrays intro

---
+++ src/main/java/arrays/JavaArrays.java
@@ -0,0 +1,13 @@
+package arrays;
+
+public class JavaArrays {
+
+    public static void main(String[] args) {
+
+        // Arrays
+        int[] arrInts = {2,4,5,8};
+
+        System.out.println(arrInts[0]);
+    }
+
+}
