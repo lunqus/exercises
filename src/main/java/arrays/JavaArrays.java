04cdf9b9e4f43984aeb62f9f1f66e2bdbad88f28
LUNKUS <sigitas@lunkus.com>
2019-01-13 09:10:59 +0200

    iterating though String and Boolean arrays and prints out those values

--- src/main/java/arrays/JavaArrays.java
+++ src/main/java/arrays/JavaArrays.java
@@ -5,15 +5,38 @@ public class JavaArrays {
     public static void main(String[] args) {
 
         // Arrays
-        int[] arrInts = {2,4,5,8};
+        int[] arrInts       = {2,4,5,8};
+        String[] arrStrings = {"Johna", "Arao", "Ines", "Olga", "Fina"};
+        boolean [] arrBools = {true, false, true, false};
 
+
+        // Retrieves values by the index
+        System.out.println(arrInts[3]);
+
+        System.out.println("------------ ------------");
+
+        // Loops through and prints int values
         for (int i = 0; i < arrInts.length; i++) {
 
             System.out.println("Item #: " + arrInts[i]);
         }
 
-        // Retrieves values by the index
-        // System.out.println(arrInts[3]);
+        System.out.println("------------ ------------");
+
+        // Prints out String values
+        for (int i = 0; i < arrStrings.length; i++) {
+
+            System.out.println("My Siblings: " + arrStrings[i]);
+        }
+
+        System.out.println("------------ ------------");
+
+        // Loops through boolean array ant prints out boolean values
+        for (int i = 0; i < arrBools.length; i++) {
+
+            System.out.println("Output: " + arrBools[i]);
+        }
+
     }
 
 }
