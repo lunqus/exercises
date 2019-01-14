ebb702f3b86ba882dba84ffd134855605d9ae152
LUNKUS <sigitas@lunkus.com>
2019-01-14 15:21:07 +0200

    considering hashmap project

---
+++ src/main/java/hashMaps/HashMapsMain.java
@@ -0,0 +1,17 @@
+package hashMaps;
+
+import java.util.HashMap;
+
+public class HashMapsMain {
+
+    public static void main(String[] args) {
+
+        HashMap users = new HashMap<>();
+
+        users.put("Kealy", 34);
+
+        System.out.println(users.get("Kealy"));
+
+
+    }
+}
