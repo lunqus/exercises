a8bacbf7e997ed8951d3c9450dc9fb63118198a8
LUNKUS <sigitas@lunkus.com>
2019-01-14 15:25:07 +0200

    using containKey at HashMap

--- src/main/java/hashMaps/HashMapsMain.java
+++ src/main/java/hashMaps/HashMapsMain.java
@@ -9,8 +9,16 @@ public class HashMapsMain {
         HashMap users = new HashMap<>();
 
         users.put("Kealy", 34);
+        users.put("Bonni", 25);
+
+        if (users.containsKey("Kealy")) {
+            System.out.println("Yeh!");
+        } else {
+            System.out.println("Nay!");
+        }
 
         System.out.println(users.get("Kealy"));
+        System.out.println(users.get("Bonni"));
 
 
     }
