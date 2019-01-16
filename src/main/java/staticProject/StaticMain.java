0492d5f2bfb75f86d733f21dc65f818a2b682967
LUNKUS <sigitas@lunkus.com>
2019-01-16 06:29:13 +0200

    Math class as utility class demo

--- src/main/java/staticProject/StaticMain.java
+++ src/main/java/staticProject/StaticMain.java
@@ -4,6 +4,11 @@ public class StaticMain {
 
     public static void main(String[] args) {
 
+        int myValue = -45;
+        int secondValue = 56;
+
+        System.out.println(Math.abs(myValue));
+        System.out.println("Minimum: " + Math.min(myValue, secondValue));
 
     }
 }
