f0b1591bdb07f0fdc218da378c565a7cde5f1479
LUNKUS <sigitas@lunkus.com>
2019-01-17 11:33:37 +0200

    Dog & Canine classes with inheritance

--- src/main/java/Interface/InterfaceTest.java
+++ src/main/java/Interface/InterfaceTest.java
@@ -4,5 +4,19 @@ public class InterfaceTest {
 
     public static void main(String[] args) {
 
+        Dog myDog = new Dog();
+
+        myDog.setName("Pyro");
+
+        System.out.println(myDog.getName());
+
     }
+
+    /**
+     *      Animal
+     *        ||
+     *      Canine
+     *        ||
+     *        Dog
+     */
 }
