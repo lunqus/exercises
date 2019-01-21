ee0013495d1182293979f074a7a8209772347b42
LUNKUS <sigitas@lunkus.com>
2019-01-21 06:29:27 +0200

    figuring out variables types

--- src/main/java/autoboxing/AutoboxingMain.java
+++ src/main/java/autoboxing/AutoboxingMain.java
@@ -2,4 +2,9 @@ package autoboxing;
 
 public class AutoboxingMain {
 
+    // Primitive:       int, chars, long, float, double, boolean, byte ...
+    // Object types:    Integer, Character, Long, Float, Double, Boolean, Byte ...
+
+
+
 }
