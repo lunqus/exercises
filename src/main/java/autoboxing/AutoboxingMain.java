93af5bf6593e910a39761773a7e18079db19f07f
LUNKUS <sigitas@lunkus.com>
2019-01-24 19:04:23 +0200

    figuring out variables types

--- src/main/java/autoboxing/AutoboxingMain.java
+++ src/main/java/autoboxing/AutoboxingMain.java
@@ -1,10 +1,23 @@
 package autoboxing;
 
+import java.util.ArrayList;
+
 public class AutoboxingMain {
 
     // Primitive:       int, chars, long, float, double, boolean, byte ...
     // Object types:    Integer, Character, Long, Float, Double, Boolean, Byte ...
 
 
+    int i = 32;
+
+    // Autoboxing happens automatically
+    ArrayList<Integer> myArray = new ArrayList();
+
+    public void setMyArray(ArrayList<Integer> myArray) {
+        this.myArray = myArray;
+        myArray.add(i);
+    }
+
+    int myItem = myArray.get(0);
 
 }
