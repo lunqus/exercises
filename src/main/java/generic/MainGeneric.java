1a4e119fe643db42fd25639cd5592498b2bb8e43
LUNKUS <sigitas@lunkus.com>
2019-01-26 16:09:10 +0200

    Login class

--- src/main/java/generic/MainGeneric.java
+++ src/main/java/generic/MainGeneric.java
@@ -2,5 +2,12 @@ package generic;
 
 public class MainGeneric {
 
+    class Login<T> {
+        private T name;
+        private T password;
+    }
+
+    public static void main(String[] args) {
+    }
 
 }
