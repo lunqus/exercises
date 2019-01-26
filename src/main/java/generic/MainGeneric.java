5f2343f1cccc84fcaeac58e42cb099e4bc9bc01b
LUNKUS <sigitas@lunkus.com>
2019-01-26 16:26:02 +0200

    Instantiating Login class and getting parameters

--- src/main/java/generic/MainGeneric.java
+++ src/main/java/generic/MainGeneric.java
@@ -1,13 +1,39 @@
 package generic;
 
-public class MainGeneric {
+class Login<T> {
+    private T name;
+    private T password;
 
-    class Login<T> {
-        private T name;
-        private T password;
+    public Login(T name, T password) {
+        this.name = name;
+        this.password = password;
     }
 
+    public T getName() {
+        return name;
+    }
+
+    public void setName(T name) {
+        this.name = name;
+    }
+
+    public T getPassword() {
+        return password;
+    }
+
+    public void setPassword(T password) {
+        this.password = password;
+    }
+}
+
+public class MainGeneric {
+
     public static void main(String[] args) {
+
+        Login<String> login = new Login<>("Paul", "password");
+
+        System.out.println(login.getName());
+
     }
 
 }
