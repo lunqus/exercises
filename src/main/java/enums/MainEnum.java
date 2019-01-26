f02003a2e499395f2cf165c20935f12381f34727
LUNKUS <sigitas@lunkus.com>
2019-01-26 08:43:11 +0200

    static final variables in use

---
+++ src/main/java/enums/MainEnum.java
@@ -0,0 +1,18 @@
+package enums;
+
+public class MainEnum {
+
+    public static final int JERRY = 1;
+    public static final int BOBBY = 2;
+    public static final int LARRY = 3;
+
+    public static void main(String[] args) {
+
+        int selectedPerson = 1;
+
+        if (selectedPerson == JERRY) {
+            System.out.println("Jerry!");
+        }
+    }
+
+}
