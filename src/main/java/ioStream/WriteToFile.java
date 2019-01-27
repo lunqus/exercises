f37ee1acd398b6ce47d917c20ce58180d82bda2c
LUNKUS <sigitas@lunkus.com>
2019-01-27 14:51:47 +0200

    created MyFile.txt with a code

--- src/main/java/ioStream/WriteToFile.java
+++ src/main/java/ioStream/WriteToFile.java
@@ -1,6 +1,26 @@
 package ioStream;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WriteToFile {
 
+    public static void main(String[] args) {
+
+        File file = new File("src/main/java/ioStream/MyTest.txt");
+
+        try ( FileWriter fileWriter = new FileWriter(file) ) {
+
+            fileWriter.write("This is amazing ... ");
+            fileWriter.close();
+
+            // Read it all back to us
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
