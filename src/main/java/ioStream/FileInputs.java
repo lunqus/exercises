6f09a40490e72a18fa231347cd94a6fc942cd32e
LUNKUS <sigitas@lunkus.com>
2019-01-27 11:50:36 +0200

    printing out a content of the File.txt

--- src/main/java/ioStream/FileInputs.java
+++ src/main/java/ioStream/FileInputs.java
@@ -1,8 +1,27 @@
 package ioStream;
 
+import java.io.File;
+import java.io.FileInputStream;
+
 public class FileInputs {
 
     public static void main(String[] args) {
 
+        File file = new File("src/main/java/ioStream/Test.txt");
+
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+
+            System.out.println("Total size to read in bytes is " + fileInputStream.available());
+
+            int content;
+
+            while ((content = fileInputStream.read()) != -1) {
+                // convert to char and display
+                System.out.print((char) content);
+            }
+
+        } catch (Exception e) {
+
+        }
     }
 }
