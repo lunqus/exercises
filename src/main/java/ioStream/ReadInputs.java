3fc907a35b04367384de5f673d9381642edecffc
LUNKUS <sigitas@lunkus.com>
2019-01-27 11:25:08 +0200

    ReadInputs class created for read was is passed inside of our console or command

--- src/main/java/ioStream/ReadInputs.java
+++ src/main/java/ioStream/ReadInputs.java
@@ -1,8 +1,20 @@
 package ioStream;
 
+import java.io.IOException;
+
 public class ReadInputs {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        byte data[] = new byte[11];
+
+        System.out.print("Enter some characters: ");
+        System.in.read(data);
+
+        System.out.print("You entered: ");
+
+        for (int i = 0; i < data.length; i++) {
+            System.out.print( (char) data[i]);
+        }
     }
 }
