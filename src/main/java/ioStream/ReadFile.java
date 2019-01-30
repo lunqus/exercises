9d1a223588e589b14a62ef73c5474497a366934b
LUNKUS <sigitas@lunkus.com>
2019-01-30 06:51:37 +0200

    added BufferedReader to read each line efficiency form the file

--- src/main/java/ioStream/ReadFile.java
+++ src/main/java/ioStream/ReadFile.java
@@ -1,6 +1,19 @@
 package ioStream;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class ReadFile { //
 
+    public static void main(String[] args) {
 
+        File file = new File("src/main/java/ioStream/Test.txt");
+
+        try (FileReader fileReader = new FileReader()) {
+
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+        }
+    }
 }
